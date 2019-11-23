167f352ce932218f4685ecf93164877d68d28c8d
mikedu <mikedutuandu@gmail.com>
2019-11-23 10:56:26 +0700

    update

--- config/settings.py
+++ config/settings.py
@@ -174,7 +174,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "statics"),
+    #os.path.join(BASE_DIR, "static_in_env"),
+    #'/var/www/static/',
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
